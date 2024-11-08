4c9f155c3127b4cc96d9bae78fea5f7a01c7a4ce
ellidwe <eli.jl.dew@gmail.com>
2024-11-08 11:02:42 -0500

    Started to add timer for minigame

--- src/Fish.java
+++ src/Fish.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 public class Fish {
 
     String name;
@@ -11,8 +10,7 @@ public class Fish {
         this.price = price;
     }
 
-    public void catchFish() {
-        Scanner s = new Scanner(System.in);
+    public String getCatchingGoalStr() {
         int randInt = (int) (Math.random() * 5);
         String goalStr = "";
 
@@ -21,15 +19,12 @@ public class Fish {
                 switch (difficulty) {
                     case 0: //If difficulty is 0
                         goalStr = "Fishing!";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
                     case 1: //If difficulty is 1
                         goalStr = "Fish are friends, not food";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
-                    case 2: //If difficulty is 2
+                    case 2://If difficulty is 2
                         goalStr = "The early fish gets the (bait) worm";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
                 }
                 break;
@@ -37,15 +32,12 @@ public class Fish {
                 switch (difficulty) {
                     case 0: //If difficulty is 0
                         goalStr = "Splish splash";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
                     case 1: //If difficulty is 1
                         goalStr = "I fish; therefore, I am";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
                     case 2: //If difficulty is 2
                         goalStr = "One fish, two fish, red fish, blue fish";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
                 }
                 break;
@@ -53,15 +45,12 @@ public class Fish {
                 switch (difficulty) {
                     case 0: //If difficulty is 0
                         goalStr = "Go fish!";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
                     case 1: //If difficulty is 1
                         goalStr = "Man I Love Fishing!!!.";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
                     case 2: //If difficulty is 2
                         goalStr = "There are plenty of fish in the sea";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
                 }
                 break;
@@ -69,15 +58,12 @@ public class Fish {
                 switch (difficulty) {
                     case 0: //If difficulty is 0
                         goalStr = "Out fishin'";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
                     case 1: //If difficulty is 1
                         goalStr = "Hook, line, and sinker";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
                     case 2: //If difficulty is 2
                         goalStr = "Born to fish, forced to talk to my wife";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
                 }
                 break;
@@ -85,27 +71,17 @@ public class Fish {
                 switch (difficulty) {
                     case 0: //If difficulty is 0
                         goalStr = "Carp diem";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
                     case 1: //If difficulty is 1
                         goalStr = "You gotta be squidding me!";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
                     case 2: //If difficulty is 2
                         goalStr = "A reel expert can tackle any problem";
-                        System.out.println("Type \"" + goalStr + "\"");
                         break;
                 }
                 break;
         }
-         
-        String userStr = s.nextLine();
-        
-        if (userStr.equalsIgnoreCase(goalStr)) {
-            System.out.println("Successful catch!");
-            //Add fish to inventory
-        } else {
-            System.out.println("Unsuccessful");
-        }
+
+        return goalStr;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
+import java.util.Date;
 public class Main {
+
     public static void main(String[] args) {
 
-        Fish fish = new Fish("fish tuah", 2, 0);
-        fish.catchFish();
+        String goalStr = "";
+        String userStr = "";
 
+        Date startTimeDate = new Date();
+        long startTime;
+
+        Scanner s = new Scanner(System.in);
+        Fish testFish = new Fish("Test Fish", 2, 0);
+
+        //Catch fish
+        goalStr = testFish.getCatchingGoalStr();
+        System.out.println("Type: " + goalStr);
+
+        startTime = startTimeDate.getTime();
+        //Gonna run out of time in class but the idea is make a loop that increments a variable that equals starttime by one per ms
+        //that passes while the loop is executing until its greater than starttime plus (secs*1000) and if the userStr is correct or incorrect
+        // in that time then break the loop or if the loop finishes say "ran out of time" probably a while loop
+        userStr = s.nextLine();
+
+        if (userStr.equalsIgnoreCase(goalStr)) {
+            System.out.println("Successful catch!");
+            //Add fish to inventory
+        } else {
+            System.out.println("Incorrect String!");
+        }
     }
 }
