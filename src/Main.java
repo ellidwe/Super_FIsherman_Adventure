5299cbc4b0ac4474d2b30e10b4fdc7b020c262ab
ellidwe <eli.jl.dew@gmail.com>
2024-11-12 11:02:15 -0500

    Readded timers, still experimenting on how to make the minigame work

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,48 @@
 import java.util.Scanner;
-import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
 
+    private static String str = "";
+
+    static TimerTask fishingMinigameTimer = new TimerTask()
+    {
+        public void run()
+        {
+
+            if(str.isEmpty())
+            {
+                System.out.println("Out of time, type anything to end.");
+            }
+        }
+    };
+
+
     public static void main(String[] args) {
-
-        String goalStr = "";
-        String userStr = "";
-
-        Date startTimeDate = new Date();
-        long startTime;
 
         Scanner s = new Scanner(System.in);
         Fish testFish = new Fish("Test Fish", 2, 0);
 
         //Catch fish
+        String goalStr;
+        String userStr = "";
+
+        Timer timer = new Timer();
+
         goalStr = testFish.getCatchingGoalStr();
         System.out.println("Type: " + goalStr);
 
-        startTime = startTimeDate.getTime();
-        //Gonna run out of time in class but the idea is make a loop that increments a variable that equals starttime by one per ms
-        //that passes while the loop is executing until its greater than starttime plus (secs*1000) and if the userStr is correct or incorrect
-        // in that time then break the loop or if the loop finishes say "ran out of time" probably a while loop
+        timer.schedule(fishingMinigameTimer, 10*1000);
+
         userStr = s.nextLine();
 
-        if (userStr.equalsIgnoreCase(goalStr)) {
-            System.out.println("Successful catch!");
-            //Add fish to inventory
-        } else {
-            System.out.println("Incorrect String!");
+            timer.cancel();
+            if (userStr.equalsIgnoreCase(goalStr)) {
+                System.out.println("Successful catch!");
+                //Add fish to inventory
+            } else {
+                System.out.println("Incorrect String!");
+            }
         }
     }
-}
